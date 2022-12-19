5622f75d854d76d1f41c3d9e1c60ce651c89c3ef
eeseohyun <jinlijinyoung@gmail.com>
2022-12-20 01:58:37 +0900

    Form으로부터 받은 값을 리스트에 추가 및 출력

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import React, { useState } from "react";
 import InsertForm from "./components/InsertForm";
+import ListView from "./components/ListView";
+
 const App = () => {
+	const [todoList, setTodoList] = useState([]); //InsertForm으로부터 전달받은 할일을 등록하고 관리할 todoList state를 선언
+	const handleInsert = (value) => {
+		setTodoList((current) => {
+			const newTodoList = [...current];
+			newTodoList.push({
+				key: new Date().getTime(), // 랜덤으로 고유번호 부여
+				value: value, // onInsert로부터 전달받은 값,
+				isCompleted: false, // 완료 처리를 위한 flag
+			});
+			return newTodoList;
+		});
+	};
+
 	return (
 		<div className="App">
-			<InsertForm onInsert={console.log} />
+			<ListView todoList={todoList} />
+			<InsertForm onInsert={handleInsert} />
 		</div>
 	);
 };
