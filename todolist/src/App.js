b9f28426a32bbbbab3f5440de42fa51d858f0ddf
eeseohyun <jinlijinyoung@gmail.com>
2022-12-20 16:32:37 +0900

    할 일 완료/삭제 기능 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 	const [todoList, setTodoList] = useState([]); //InsertForm으로부터 전달받은 할일을 등록하고 관리할 todoList state를 선언
 	const handleInsert = (value) => {
 		setTodoList((current) => {
-			const newTodoList = [...current];
+			const newTodoList = [...current]; //안정성
 			newTodoList.push({
 				key: new Date().getTime(), // 랜덤으로 고유번호 부여
 				value: value, // onInsert로부터 전달받은 값,
@@ -16,10 +16,28 @@ const App = () => {
 			return newTodoList;
 		});
 	};
+	const handleComplete = (index) => {
+		setTodoList((current) => {
+			const newTodoList = [...current];
+			newTodoList[index].isCompleted = true;
+			return newTodoList;
+		});
+	};
+	const handelRemove = (index) => {
+		setTodoList = (current) => {
+			const newTodoList = [...current];
+			newTodoList.splice(index, 1);
+			return newTodoList;
+		};
+	};
 
 	return (
 		<div className="App">
-			<ListView todoList={todoList} />
+			<ListView
+				todoList={todoList}
+				onComplete={handleComplete}
+				onRemove={handelRemove}
+			/>
 			<InsertForm onInsert={handleInsert} />
 		</div>
 	);
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App isLoggedIn={false} />, document.getElementById("root"));
 
